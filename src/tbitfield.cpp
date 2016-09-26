6f2139200ab8d160adc13523b0ed7913f4a6d793
Vika Sannikova <vikasannikova1998@gmail.com>
2016-09-26 18:45:35 +0300

    добавила деструктор и некоторые методы

--- src/tbitfield.cpp
+++ src/tbitfield.cpp
@@ -17,31 +17,43 @@ TBitField::TBitField(int len)
 
 }
 
-TBitField::TBitField(const TBitField &bf) // конструктор копирования
+TBitField::TBitField(const TBitField &bf) // конструктор копирования (инициализатор???)
 {
+	BitLen = bf.BitLen;  //внутренней переменной присваиваем значение битового поля максимальнойдлины
+	MemLen = bf.MemLen;  //кол-во элементов для предоставления битового поля
+	pMem = new TELEM[MemLen];  //выделение памяти под битовое поле
+	for (int i = 0; i < MemLen; i++)
+		pMem[i] = bf.pMem[i]; //копируем элементы из вводимого числа в память для битового поля
 }
 
 TBitField::~TBitField()
 {
+	delete[] pMem;
 }
 
 int TBitField::GetMemIndex(const int n) const // индекс памяти дляинта в котором есть n
 {
+	return n/(sizeof(TELEM) * 8); //получаем индекс инта, к котором хранится наше число. для 35--1. 8 это число бит в бите и умножаем на 2
 }
 
 TELEM TBitField::GetMemMask(const int n) const // битовая маска для бита n
 {
+	return 1 << (n % (sizeof(TELEM) * 8));	//сдвигаем 1 на остаток от деления на 32. для 35-3 
 }
 
 // доступ к битам битового поля
 
 int TBitField::GetLength(void) const // получить длину (к-во битов)
 {
-  return 0;
+	return BitLen;
 }
 
 void TBitField::SetBit(const int n) // установить сдвиг бит
 {
+	if (n >= 0 && n < BitLen)
+	{
+		pMem[GetMemIndex(n)] |= GetMemMask(n); //элементу массива pMem с индексом инта в котором хранится наше число присваиваем старое значение и объединяем с маской
+	}
 }
 
 void TBitField::ClrBit(const int n) // очистить бит
